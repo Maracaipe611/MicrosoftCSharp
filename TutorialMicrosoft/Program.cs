6b5828ac9bca39e227b422cd008435a012bc508f
Lucas <maracaipelucas@gmail.com>
2020-09-01 15:53:50 -0300

    initial commit

---
+++ TutorialMicrosoft/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TutorialMicrosoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacciNumbers = new List<int> { 0, 1 };
+
+            var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+            var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+            int prox;
+
+
+            for(int fibo = 1; fibo <= 20; fibo++)
+            {
+                prox = previous + previous2;
+                Console.WriteLine($"{fibo} = {previous + previous2}");
+                previous = previous2;
+                previous2 = prox;
+            }
+        }
+    }
+}
